dca0f9f31b163e0e41b3e58dd2b6f8a06dd532e6
d <dmitriypospelov93@gmail.com>
2024-06-24 17:53:48 +0700

    Init

--- src/main/java/com/effectivemobile/csvgen/CsvGenApplication.java
+++ src/main/java/com/effectivemobile/csvgen/CsvGenApplication.java
@@ -3,6 +3,7 @@ package com.effectivemobile.csvgen;
 import com.effectivemobile.csvgen.annotation.ExcludeColumn;
 import com.effectivemobile.csvgen.dto.Report;
 import com.effectivemobile.csvgen.service.CsvService;
+import com.effectivemobile.csvgen.utils.CsvUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -14,8 +15,9 @@ public class CsvGenApplication {
 
     static CsvService csvService;
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        csvService = new CsvService();
+    public static void main(String[] args) throws IOException {
+        CsvUtils csvUtils = new CsvUtils();
+        csvService = new CsvService(csvUtils);
         csvService.makeReport("src/main/resources/report/test1.csv", List.of(new Report("Test", "Test1"), new Report("Test", "Test1")));
         SpringApplication.run(CsvGenApplication.class, args);
     }
--- src/main/java/com/effectivemobile/csvgen/service/CsvService.java
+++ src/main/java/com/effectivemobile/csvgen/service/CsvService.java
@@ -3,15 +3,17 @@ package com.effectivemobile.csvgen.service;
 
 import com.effectivemobile.csvgen.dto.Report;
 import com.effectivemobile.csvgen.utils.CsvUtils;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class CsvService {
 
-    CsvUtils csvUtils;
+    private final CsvUtils csvUtils;
 
     public void makeReport(String filePath, List<Report> list) throws IOException {
         csvUtils.writeDataLineByLine(filePath, list);
